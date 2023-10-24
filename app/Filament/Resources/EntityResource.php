7e6f0762c7f846dc1c8c84c45495cd73382f2f50
alexis <alexis@glanum.com>
2023-10-24 12:32:50 +0200

    Adding Entities names and types to filament and display them

---
+++ app/Filament/Resources/EntityResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EntityResource\Pages;
+use App\Filament\Resources\EntityResource\RelationManagers;
+use App\Models\Entity;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EntityResource extends Resource
+{
+    protected static ?string $model = Entity::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Select::make('type')->options([
+                    'Affilié' => 'Affilié',
+                    'Producteur' => 'Producteur',
+                    'Client' => 'Client',
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('type'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEntities::route('/'),
+            'create' => Pages\CreateEntity::route('/create'),
+            'edit' => Pages\EditEntity::route('/{record}/edit'),
+        ];
+    }
+}
