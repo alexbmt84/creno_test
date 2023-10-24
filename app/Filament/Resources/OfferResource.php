7c7b9f74be8a6687d9060b2a2dfe4a078ae15d05
alexis <alexis@glanum.com>
2023-10-24 16:36:34 +0200

    Adding Offers to Filament

---
+++ app/Filament/Resources/OfferResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OfferResource\Pages;
+use App\Filament\Resources\OfferResource\RelationManagers;
+use App\Models\Offer;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OfferResource extends Resource
+{
+    protected static ?string $model = Offer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('product_id')->relationship(name: 'product', titleAttribute: 'name')->required(),
+                Select::make('category_id')->relationship(name: 'category', titleAttribute: 'name')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('product.name'),
+                Tables\Columns\TextColumn::make('category.name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOffers::route('/'),
+            'create' => Pages\CreateOffer::route('/create'),
+            'edit' => Pages\EditOffer::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Offer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Offer extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function product(): BelongsTo {
+
+        return $this->belongsTo(Product::class);
+
+    }
+
+     public function category(): BelongsTo {
+
+        return $this->belongsTo(Category::class);
+
+    }
+
+}
